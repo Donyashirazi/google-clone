831febfa01465c01fd04ed7f103aef2fe727f493
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-10-04 22:09:08 +0330

    js start

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let sideBar = document.querySelector("#sideBar");
 let sideNavigation = document.querySelector(".sideNavigation");
+let gridContainer = document.querySelector(".gridContainer");
 
-sideBar.addEventListener("click", function () {
-  sideNavigation.classList.toggle("active");
-});
+// sideBar.addEventListener("click", function () {
+//   sideNavigation.classList.toggle("active");
+// });
+
+sideBar.onclick = function () {
+  setTimeout(() => {
+    gridContainer.classList.toggle("active");
+  }, 1000);
+};
