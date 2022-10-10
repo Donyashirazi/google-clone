1ee3c942162e6dfe4ef9e6e01717ae0d7dcb2a6c
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-10-11 01:10:10 +0330

    new-flex

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const { doc } = require("prettier");
+
 let sideBar = document.querySelector("#sideBar");
 let sideNavigation = document.querySelector(".sideNavigation");
 let gridContainer = document.querySelector(".gridContainer");
+let firstMenu = document.querySelector("#firstMenu");
+let secondMenu = document.querySelector("#secondMenu");
+let fourthMenu = document.querySelector("#thirdMenu");
 
 // sideBar.addEventListener("click", function () {
 //   sideNavigation.classList.toggle("active");
@@ -10,10 +15,4 @@ sideBar.onclick = function () {
   setTimeout(() => {
     gridContainer.classList.toggle("active");
   }, 500);
-};
-
-document.onclick = function (screenClick) {
-  if (screenClick.target.id !== gridContainer) {
-    gridContainer.classList.remove("active");
-  }
 };
