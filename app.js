36cb3d622ebaff09935d14d8a2be937a8d83b484
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-10-17 00:13:25 +0330

    changed

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 const { doc } = require("prettier");
 
-let sideBar = document.querySelector("#sideBar");
-let sideNavigation = document.querySelector(".sideNavigation");
-let gridContainer = document.querySelector(".gridContainer");
 let firstMenu = document.querySelector("#firstMenu");
-let secondMenu = document.querySelector("#secondMenu");
-let fourthMenu = document.querySelector("#thirdMenu");
 
-// sideBar.addEventListener("click", function () {
-//   sideNavigation.classList.toggle("active");
-// });
-
-sideBar.onclick = function () {
-  setTimeout(() => {
-    gridContainer.classList.toggle("active");
-  }, 500);
-};
+firstMenu.addEventListener("click", function () {
+  alert("123");
+});
