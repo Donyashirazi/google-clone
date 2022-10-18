dc43e43e76db2e7cea84a43d898cd1ad11ec5071
Donya Shirazi <donya_rezaii97@yahoo.com>
2022-10-18 22:27:53 +0330

    all but one

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 // const { doc } = require ("prettier");
 
+// const { firstAid } = require("fontawesome");
+
 let nav = document.querySelectorAll(".nav");
 let sideMenu = document.querySelectorAll(".sideMenu");
 let sideNavigation = document.querySelector("#sideNavigation");
 let sideBar = document.querySelector("#sideBar");
 let gridContainer = document.querySelector(".gridContainer");
+let first = document.querySelector("#first");
+let second = document.querySelector("#second");
+let fourth = document.querySelector("#fourth");
 
 for (let x = 0; x < nav.length; x++) {
-  nav[x].addEventListener("click", function () {
+  nav[x].addEventListener("click", function abc() {
     sideMenu[x].classList.toggle("active");
   });
+  document.onclick = function (screenClick) {
+    if (screenClick.target.getAttribute("class") !== "sideMenu active") {
+      nav[x].removeEventListener("click", abc);
+    }
+  };
 }
